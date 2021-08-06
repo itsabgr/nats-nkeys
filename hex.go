f256cd6c3fa359c0de0d56bfc38eb1b42c3f8aca
abgr <tbpmtbpm@gmail.com>
2021-08-06 18:41:53 +0430

    export

--- hex.go
+++ hex.go
@@ -2,25 +2,25 @@ package nkeys
 
 import "encoding/hex"
 
-type hexEncoding struct{}
+type HexEncoding struct{}
 
-func (_ hexEncoding) Decode(d, s []byte) (int, error) {
+func (_ HexEncoding) Decode(d, s []byte) (int, error) {
 	return hex.Decode(d, s)
 }
-func (_ hexEncoding) DecodeString(s string) ([]byte, error) {
+func (_ HexEncoding) DecodeString(s string) ([]byte, error) {
 	return hex.DecodeString(s)
 }
 
-func (_ *hexEncoding) DecodedLen(x int) int {
+func (_ *HexEncoding) DecodedLen(x int) int {
 	return hex.DecodedLen(x)
 }
-func (_ hexEncoding) Encode(d, s []byte) int {
+func (_ HexEncoding) Encode(d, s []byte) int {
 	return hex.Encode(d, s)
 }
-func (_ hexEncoding) EncodeToString(s []byte) string {
+func (_ HexEncoding) EncodeToString(s []byte) string {
 	return hex.EncodeToString(s)
 }
 
-func (_ hexEncoding) EncodedLen(x int) int {
+func (_ HexEncoding) EncodedLen(x int) int {
 	return hex.EncodedLen(x)
 }
